fa8a17582cf7422482617b2555050ae2490dd513
Sihle Kambula <kambulasihle@gamil.com>
2024-02-10 20:34:58 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/mindr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mindr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mindr',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Wrapper(),
+    );
+  }
+}
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text('Mindr'),
+    ));
+  }
+}
