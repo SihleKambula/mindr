3a07be0506ea3a0b509360fdd47d1da32d702bbb
Sihle Kambula <kambulasihle@gamil.com>
2024-02-22 23:24:24 +0200

    add anon user

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mindr/wrapper.dart';
 import 'package:go_router/go_router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Mindr());
 }
 
--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,12 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
+  Future<void> loginAnon() async {
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      print(userCredential);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          print("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          print("Unknown error.");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
-      child: Text('Mindr'),
+      child: ElevatedButton(onPressed: loginAnon, child: const Text('Log in')),
     ));
   }
 }
