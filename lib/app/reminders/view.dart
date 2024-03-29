1d3898b31f59c02eeae2a8e8b828ede61d548e54
Sihle Kambula <kambulasihle@gamil.com>
2024-03-29 21:06:47 +0200

    created the reminders screen

--- lib/app/reminders/view.dart
+++ lib/app/reminders/view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconsax/iconsax.dart';
 
 class Reminders extends StatefulWidget {
   const Reminders({super.key});
@@ -11,10 +13,39 @@ class _RemindersState extends State<Reminders> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: const Center(
-        child: Text('Reminders Screen'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Reminders'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('Recording'),
+          child: const Icon(Iconsax.microphone),
+        ),
+        backgroundColor: Colors.grey[100],
+        body: ListView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            final item = index;
+            return InkWell(
+              onTap: null,
+              child: Dismissible(
+                key: Key('hello'),
+                onDismissed: (direction) {
+                  print(index);
+                },
+                background: Container(color: Colors.red),
+                child: const Card(
+                  elevation: 0,
+                  child: ListTile(
+                    title: Text('The Reminder'),
+                    tileColor: Colors.white,
+                    subtitle: Text('The Date'),
+                    trailing: Icon(Iconsax.arrow_circle_right),
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
